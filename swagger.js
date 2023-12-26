ccf13ab6ab9b6cd17e9f3c569689ae59c7e0ba93
Jay <jayantchoudhary271@gmail.com>
2023-12-26 12:44:56 +0530

    Code fix

--- swagger.js
+++ swagger.js
@@ -5,14 +5,14 @@ const doc = {
     title: "My API",
     description: "API",
   },
-  host: 'localhost:4000'
+  host: "localhost:4000",
 };
 
 const outputFile = "./swagger-output.json";
 const routes = [
   "./routes/auth.js",
   "./routes/pages.js",
-  "./routes//subPages.js"
+  "./routes//subPages.js",
 ];
 
 swaggerAutogen(outputFile, routes, doc);
